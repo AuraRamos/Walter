4733a9df8bdf0543870cb72f490645ecb0551ee2
AuraRamos <aramos40@ucol.mx>
2025-03-04 09:54:13 -0600

    120224.c

---
+++ ‎120224.c
@@ -0,0 +1,18 @@
+/// escriba un programa que imprima los dias de la semana
+//en dos funciones, uno que imprima los dias habiles y otro los dias fin de semana
+
+void entresemana();
+void findesemana();
+
+void main(){
+    entresemana();
+    findesemana();
+}
+
+void entresemana();{
+    printf("Lunes\nMartes\nMiercoles\nJueves\nViernes")
+}
+
+void findesemana();{
+    printf("Sabado\nDomingo")
+}
